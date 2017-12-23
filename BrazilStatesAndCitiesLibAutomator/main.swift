5012607d838c544fa1c6dd85d39044e33140bd6a
Munir Wanis <fallen.angel.pp@gmail.com>
2017-12-23 12:57:10 -0200

    ➕  Adds Implementation

--- BrazilStatesAndCitiesLibAutomator/main.swift
+++ BrazilStatesAndCitiesLibAutomator/main.swift
@@ -8,5 +8,76 @@
 
 import Foundation
 
-print("Hello, World!")
+struct States: Decodable {
+    var estados: [State]
+}
 
+struct State: Decodable {
+    var sigla: String
+    var nome: String
+    var cidades: [String]
+}
+
+let fileToRead = "estados-cidades.json"
+let fileToWrite = "BrazilStates.swift"
+
+if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
+    
+    let fileURL = dir.appendingPathComponent(fileToRead)
+    let fileURLToWrite = dir.appendingPathComponent(fileToWrite)
+    
+    //reading
+    do {
+        let json = try String(contentsOf: fileURL, encoding: .utf8).data(using: .utf8)
+        let statesUnsorted = try JSONDecoder().decode(States.self, from: json!)
+        let states = statesUnsorted.estados.sorted(by: { $0.sigla < $1.sigla })
+        
+        let tab = "    "
+        
+        // MARK: - Enum declaration
+        var statesEnum = "enum BrazilStates: String {\n"
+        
+        states.forEach {
+            statesEnum.append("\(tab)case \($0.sigla.lowercased()) = \"\($0.sigla.uppercased())\"\n")
+        }
+        
+        statesEnum.append("\n")
+        
+        // MARK: - Variable generation method
+        
+        statesEnum.append("\(tab)var code: String {\n")
+        statesEnum.append("\(tab)\(tab)return self.rawValue\n")
+        statesEnum.append("\(tab)}\n\n")
+        
+        statesEnum.append("\(tab)var name: String {\n")
+        statesEnum.append("\(tab)\(tab)let stateInformation = getStateInformation()\n")
+        statesEnum.append("\(tab)\(tab)return stateInformation.name\n")
+        statesEnum.append("\(tab)}\n\n")
+        
+        statesEnum.append("\(tab)var cities: [String] {\n")
+        statesEnum.append("\(tab)\(tab)let stateInformation = getStateInformation()\n")
+        statesEnum.append("\(tab)\(tab)return stateInformation.cities\n")
+        statesEnum.append("\(tab)}\n\n")
+        
+        // MARK: - getStateInformation() method
+        
+        statesEnum.append("\(tab)private func getStateInformation() -> (name: String, cities: [String]) {\n")
+        statesEnum.append("\(tab)\(tab)switch self {\n")
+        
+        states.forEach {
+            statesEnum.append("\(tab)\(tab)\(tab)case .\($0.sigla.lowercased()): return (\"\($0.nome)\", \($0.cidades))\n")
+        }
+        
+        statesEnum.append("\(tab)\(tab)\(tab)default: return (\"\", [])\n")
+        statesEnum.append("\(tab)\(tab)}\n")
+        statesEnum.append("\(tab)}\n")
+        
+        statesEnum.append("}")
+        
+        try statesEnum.write(to: fileURLToWrite, atomically: false, encoding: .utf8)
+    }
+    catch {
+        print("Deu merda.")
+        print(error)
+    }
+}
