28eeb675820f8f8de65ed81c209f8e09f043c2a9
Munir Wanis <fallen.angel.pp@gmail.com>
2017-12-23 12:13:16 -0200

    Initial Commit

---
+++ BrazilStatesAndCitiesLibAutomator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  BrazilStatesAndCitiesLibAutomator
+//
+//  Created by Munir Wanis on 23/12/17.
+//  Copyright © 2017 Munir Wanis. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
